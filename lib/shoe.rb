8d9b11ff83781d07aa35f833da143d79cf942226
wolfene2001 <wolfene2001@yahoo.com>
2019-12-17 01:33:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,14 +3,14 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-#  def initialize(brand)
-#    @brand = brand
-#  end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  def initialize(brand)
+    @brand = brand
   end
+
+#  def cobble
+#    self.condition = "new"
+#    puts "Your shoe is as good as new!"
+#  end
 
   def brand
     @brand = brand
