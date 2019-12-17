724429f999d549a0d00f92e112e21cccc4049a59
wolfene2001 <wolfene2001@yahoo.com>
2019-12-17 01:49:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.any? { |b| b==brand}
   end
 
 #  def cobble
@@ -12,8 +13,8 @@ class Shoe
 #    puts "Your shoe is as good as new!"
 #  end
 
-  def brand
-    @brand = brand
-    BRANDS << brand
-  end
+#  def brand
+#    @brand = brand
+#    BRANDS << brand
+#  end
 end
